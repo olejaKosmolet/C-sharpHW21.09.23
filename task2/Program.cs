2d2f6f6ee99baea831071a4f4f55bcc9cad0e677
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-22 00:40:47 +0300

    Added code2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task13
+
+int number = Convert.ToInt32(Console.ReadLine());
+string result = number.ToString();
+int num1;
+
+if (number / 10 == 0 || number / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else 
+{
+    num1 = (result[2] - '0');
+    Console.WriteLine(num1);
+}
