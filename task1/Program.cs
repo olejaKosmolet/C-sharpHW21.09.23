8034974dbe60e308e8465c28528185beebf9e23a
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-21 22:09:14 +0300

    Added code1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// task 10
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num1 = number / 10;
+int num2 = num1 % 10;
+
+Console.WriteLine(num2);
