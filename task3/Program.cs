b5acf1888cef959656353e071bd012c9bf4e3735
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-22 01:16:18 +0300

    Added code3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Task 15
+Console.Write("Для проверки выходного дня введите число соответствующее дню недели: ")
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("нет");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine("да");
+        break;
+}
