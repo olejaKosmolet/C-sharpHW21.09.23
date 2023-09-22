e82990df98ea006a55e842652711920bee3957bc
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-22 22:20:24 +0300

    Added code2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,7 +4,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 string result = number.ToString();
 int num1;
 
-if (number / 10 == 0 || number / 100 == 0)
+if (number<100)
 {
     Console.WriteLine("Третьей цифры нет!");
 }
